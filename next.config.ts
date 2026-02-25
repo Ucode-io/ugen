31fbfcad889bb8a677188a5c046bd339a591c48c
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2026-02-25 12:44:28 +0500

    change config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: {},
-  },
+  experimental: {},
 };
 
 const withNextIntl = createNextIntlPlugin("./src/shared/lib/i18n/request.ts");
