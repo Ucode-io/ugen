6bb8616ef9a72f5593d63f7f1c67ae13bf866dec
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2026-05-15 12:28:17 +0500

    fix: image preview

--- src/widgets/projects/ui/projects-grid.tsx
+++ src/widgets/projects/ui/projects-grid.tsx
@@ -44,7 +44,7 @@ export const ProjectsGrid = ({ projects, folders = [], readOnly = false, variant
           <>
             <div className="relative aspect-[16/9] w-full overflow-hidden rounded-2xl border border-border-subtle bg-bg-sidebar transition-all flex items-center justify-center">
               {proj.image ? (
-                <img src={proj.image} alt={proj.name} className="h-full w-full object-cover" />
+                <img src={proj.image} alt={proj.name} className="h-full w-full object-cover object-top" />
               ) : (
                 <div className="flex h-full w-full items-center justify-center text-border-subtle group-hover:text-text-muted transition-colors">
                   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none"><path fill="currentColor" fillRule="evenodd" d="M13.02.088c-1.458.366-2.342 1.629-2.22 3.17.02.248.153 1.03.294 1.738.24 1.207.257 1.362.256 2.504-.001 1.386-.069 1.745-.506 2.687-.66 1.424-1.79 2.02-3.02 1.592-.499-.174-1.066-.756-1.26-1.295-.36-.996-.332-1.984.126-4.45.438-2.353.428-3.416-.04-4.373C6.331 1.01 5.62.429 4.847.187 4.244-.003 3.277.03 2.703.26c-.582.233-1.145.7-1.587 1.32C.396 2.583.075 3.74.079 5.308.08 5.845.11 6.412.148 6.57c.05.214.04.42-.036.8-.167.834-.143 2.901.043 3.8.473 2.288 1.508 3.962 3.134 5.069.718.49 2.172 1.127 3.225 1.415 2.037.557 4.037.438 5.629-.334 2.885-1.4 5.063-4.746 5.734-8.812.164-.992.164-3.032 0-3.964-.431-2.458-1.58-4.05-3.207-4.448-.512-.125-1.172-.128-1.65-.008" clipRule="evenodd" /></svg>
