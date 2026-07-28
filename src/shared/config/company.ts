e3b03dbb48c79f3b32229468db2dbc81b8297974
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2026-07-28 18:53:36 +0500

    change phone number

--- src/shared/config/company.ts
+++ src/shared/config/company.ts
@@ -5,6 +5,6 @@ export const COMPANY_INFO = {
     "Tashkent city, Yunusabad district, Firdavsiy MCC, Little Automobile Ring Road street, home 81.",
   tin: "311 320 023",
   businessEmail: "support@ucode.co",
-  businessPhone: "+998998650109",
+  businessPhone: "+1 (650) 420-2182",
   website: "https://ucode.co",
 } as const;
